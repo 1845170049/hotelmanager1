23ad6cf61f498a996d52a823f6c47192703fbd2f
1845170049 <1845170049@qq.com>
2020-02-05 14:15:07 +0800

    第一次上传项目

---
+++ src/test/com/newer/test/Hote1Test.java
@@ -0,0 +1,15 @@
+package com.newer.test;
+
+import com.newer.mapper.RoomsMapper;
+import com.newer.util.SqlSessionUtil;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+public class Hote1Test {
+    @Test
+    public void tsetfind(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        RoomsMapper roomsMapper = sqlSession.getMapper(RoomsMapper.class);
+        roomsMapper.findTypes().forEach(roomstype-> System.out.println(roomstype));
+    }
+}
